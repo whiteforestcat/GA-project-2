8381fd8037f09be8cf4bd0ec147164dc979c0441
mrmuhdamir <whiteforestcat96@gmail.com>
2023-01-24 16:17:08 +0800

    create modal (with bug)

--- starter-code/src/components/Animals.jsx
+++ starter-code/src/components/Animals.jsx
@@ -3,10 +3,13 @@ import { useState } from "react";
 import useFetch from "../hooks/useFetch";
 import Button from "./Button";
 import SomeContext from "../context/some-context";
+import ImageModal from "../modals/ImageModal";
 
 const Animals = () => {
-  const [image, setImage] = useState("");
+  const [image, setImage] = useState([]);
+  const [largeImage, setLargeImage] = useState([]);
   const [store, setStore] = useState([]);
+  const [popUp, setPopUp] = useState(false);
 
   const ctx = useContext(SomeContext);
 
@@ -30,12 +33,26 @@ const Animals = () => {
           output.hits[4].previewURL,
           output.hits[5].previewURL,
         ]);
+
+        setLargeImage([
+          ...largeImage,
+          output.hits[0].largeImageURL,
+          output.hits[1].largeImageURL,
+          output.hits[2].largeImageURL,
+          output.hits[3].largeImageURL,
+          output.hits[4].largeImageURL,
+          output.hits[5].largeImageUR,
+        ]);
       });
   };
 
   useEffect(() => {
     generateData();
   }, []);
+
+  const triggerPopUp = () => {
+    setPopUp(!popUp);
+  };
 
   return (
     <>
@@ -44,12 +61,26 @@ const Animals = () => {
         <img src={element} />;
       })} */}
       <div>
-        <img src={image[0]} />
+        <img src={image[0]} onClick={triggerPopUp} />
         <Button
           favourites={ctx.favourites}
           setFavourites={ctx.setFavourites}
           image={image[0]}
         />
+        {popUp && (
+          <ImageModal image={largeImage[0]} triggerPopUp={triggerPopUp} />
+        )}
+      </div>
+      <div>
+        <img src={image[1]} onClick={triggerPopUp} />
+        <Button
+          favourites={ctx.favourites}
+          setFavourites={ctx.setFavourites}
+          image={image[1]}
+        />
+        {popUp && (
+          <ImageModal image={largeImage[2]} triggerPopUp={triggerPopUp} />
+        )}
       </div>
       <img src={image[1]} />
       <img src={image[2]} />
--- starter-code/src/modals/ImageModal.jsx
+++ starter-code/src/modals/ImageModal.jsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import ReactDom from "react-dom"
-
+import React from "react";
+import ReactDom from "react-dom";
+import styles from "./ImageModal.module.css";
 
 // create Overlay Component here
 
 const Overlay = (props) => {
-
-}
-
-
-
+  return (
+    <div className={styles.backdrop} onClick={props.triggerPopUp}>
+      <div className={`${styles.board} ${styles.modal}`}>
+        <img
+          src={props.image}
+          style={{ width: "50%" }}
+          //   onClick={props.triggerPopUp}
+        />
+        {/* {window.addEventListener("click", props.triggerPopUp)} */}
+      </div>
+    </div>
+  );
+};
 
 const ImageModal = (props) => {
-    return (
-        <>
-            {ReactDom.createPortal(
-                <Overlay/>, document.querySelector("#modal-root")
-            )}
-        </>
-    );
+  return (
+    <>
+      {ReactDom.createPortal(
+        <Overlay image={props.image} triggerPopUp={props.triggerPopUp} />,
+        document.querySelector("#modal-root")
+      )}
+    </>
+  );
 };
 
 export default ImageModal;
